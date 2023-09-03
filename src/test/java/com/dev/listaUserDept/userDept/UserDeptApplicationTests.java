f2dc1b4b9159c4ae8be6a7f5901ebc944e2b7a82
samaelsimoes <samael.simoes@gmail.com>
2023-09-03 00:40:26 -0300

    fix criado-projeto-indicador-filme

---
+++ src/test/java/com/dev/listaUserDept/userDept/UserDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.listaUserDept.userDept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
